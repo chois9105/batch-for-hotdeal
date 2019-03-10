0e9af4d6d108cedf0e6ca7a7435afca9d1d7add3
chois9105 <chois9105@gmail.com>
2019-03-11 00:40:20 +0900

    Add spring schedule settings

--- src/main/java/com/batch/qpanion/QpanionApplication.java
+++ src/main/java/com/batch/qpanion/QpanionApplication.java
@@ -1,12 +1,38 @@
 package com.batch.qpanion;
 
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 
 @SpringBootApplication
+@EnableScheduling
 public class QpanionApplication {
+
+    @Autowired
+    public JobLauncher jobLauncher;
+
+    @Autowired
+    public Job job;
+
     public static void main(String[] args) {
         SpringApplication.run(QpanionApplication.class, args);
+    }
+
+    @Scheduled(cron = "${batch.cron}")
+    public void excuteJob() throws Exception{
+
+        JobParameters params = new JobParametersBuilder()
+                .addString("JobID", String.valueOf(System.currentTimeMillis()))
+                .toJobParameters();
+
+        jobLauncher.run(job, params);
+
     }
 
 }
--- src/main/java/com/batch/qpanion/config/BatchConfig.java
+++ src/main/java/com/batch/qpanion/config/BatchConfig.java
@@ -2,11 +2,10 @@ package com.batch.qpanion.config;
 
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
-import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
-import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
-import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.*;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
